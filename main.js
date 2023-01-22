23ccd0da5b102aebd69e98fec61f809e29196343
iliko13 <107374131+iliko13@users.noreply.github.com>
2023-01-22 21:19:25 +0400

    shesworeba kodis

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -15,17 +16,15 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too High";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
     }
-  } else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too Low";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Too High" : "Too Low";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
