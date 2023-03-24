629a34b663eb0d24c3213f393f5206771f5f53e7
iliko13 <107374131+iliko13@users.noreply.github.com>
2023-03-24 14:40:24 +0400

    13

--- main.js
+++ main.js
@@ -1,70 +1,21 @@
 "use strict";
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
+const calcTempAmplitude = function (t1, t2) {
+  const temp = t1.concat(t2);
+  console.log(temp);
 
-//Asynchronous js
+  let max = temp[0];
+  let min = temp[0];
 
-// const request = new XMLHttpRequest();
-// request.open("GET", "https://restcountries.com/v3.1/name/georgia"); //API
-// request.send();
+  for (let i = 0; i < temp.length; i++) {
+    const curTemp = temp[i];
+    if (typeof curTemp !== "number") continue;
 
-// request.addEventListener("load", function () {
-//   const [data] = JSON.parse(this.responseText); //ამით მონაცემები გავპარსეთ და შესაბამისად გამოვიტანეთ კონსოლში
-//   console.log(data);
-
-//   const html = `<article class="country">
-//           <img class="country__img" src="${data.flags.svg}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data.name.common}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(
-//               +data.population / 1000000
-//             ).toFixed(1)} people</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages}</p>
-//             <p class="country__row"><span>💰</span>${data.currencies}</p>
-//           </div>
-//         </article>`;
-//   console.log(data.currencies);
-//   countriesContainer.insertAdjacentHTML("beforeend", html);
-//   countriesContainer.style.opacity = 1;
-// });
-//Object.values(user);
-
-////////////////////////////////////
-// const request = fetch("https://restcountries.com/v3.1/name/georgia");
-// console.log(request);
-
-// const getCountryData = function (country) {
-//   fetch(`https://restcountries.com/v3.1/name/${country}`)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data[0]));
-// };
-
-// getCountryData("georgia");
-
-//promise
-// const lotteryPromise = new Promise(function (resolve, reject) {
-//   console.log("Lotter draw is happening");
-//   setTimeout(function () {
-//     if (Math.random() >= 0.5) {
-//       resolve("You Win");
-//     } else {
-//       reject(new Error("You lost your money"));
-//     }
-//   }, 2000);
-// });
-
-// lotteryPromise
-//   .then((res) => console.log(res))
-//   .catch((err) => console.error(err));
-
-//
-const whereAmI = async function (country) {
-  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-  const data = await res.json();
-  console.log(data[0]);
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max);
+  console.log(min);
 };
-whereAmI("georgia");
-console.log("first"); //აქ პირველი ეს გამოვა რადგან ბექგრაუნდზეა გაშვებული ასინქრონული ფუნცქია ფეტჩის საშუალებით
+
+calcTempAmplitude([3, 4, -5], [0, 3, 4]);
--- second.js
+++ second.js
@@ -110,7 +110,7 @@ const ages2 = [];
 
 for (let i = 0; i < years.length; i++) {
   ages[i] = 2037 - years[i];
-  ages2.push(2037 - years[i]);
+  ages2.push(2037 - years[i]); //მეორენაირად
 }
 
 console.log(ages);
@@ -1477,3 +1477,68 @@ const acc1 = new Account("ilo", "EUR", 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 console.log(acc1);
+
+//Asynchronous js
+
+// const request = new XMLHttpRequest();
+// request.open("GET", "https://restcountries.com/v3.1/name/georgia"); //API
+// request.send();
+
+// request.addEventListener("load", function () {
+//   const [data] = JSON.parse(this.responseText); //ამით მონაცემები გავპარსეთ და შესაბამისად გამოვიტანეთ კონსოლში
+//   console.log(data);
+
+//   const html = `<article class="country">
+//           <img class="country__img" src="${data.flags.svg}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies}</p>
+//           </div>
+//         </article>`;
+//   console.log(data.currencies);
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   countriesContainer.style.opacity = 1;
+// });
+//Object.values(user);
+
+////////////////////////////////////
+// const request = fetch("https://restcountries.com/v3.1/name/georgia");
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data[0]));
+// };
+
+// getCountryData("georgia");
+
+//promise
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log("Lotter draw is happening");
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve("You Win");
+//     } else {
+//       reject(new Error("You lost your money"));
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
+
+//
+const whereAmI = async function (country) {
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const data = await res.json();
+  console.log(data[0]);
+};
+whereAmI("georgia");
+console.log("first"); //აქ პირველი ეს გამოვა რადგან ბექგრაუნდზეა გაშვებული ასინქრონული ფუნცქია ფეტჩის საშუალებით
